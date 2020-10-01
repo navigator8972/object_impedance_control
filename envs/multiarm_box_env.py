Fix jacobian issue and some cleanups.

envs/multiarm_box_env.py
@@ -61,7 +61,6 @@ class MultiArmBoxEnv(gym.Env):
         arm2_trq = action[self.nJointsPerArm:2*self.nJointsPerArm] + arm2_gravity - np.array(arm2_joint_state[1]) * damping
         arm3_trq = action[2*self.nJointsPerArm:3*self.nJointsPerArm] + arm3_gravity - np.array(arm3_joint_state[1]) * damping
        
-        
         #apply action
         p.setJointMotorControlArray(self.arm1Uid, range(7), p.TORQUE_CONTROL, forces=arm1_trq)
         p.setJointMotorControlArray(self.arm2Uid, range(7), p.TORQUE_CONTROL, forces=arm2_trq)
@@ -99,10 +98,17 @@ class MultiArmBoxEnv(gym.Env):
         jac_t_3, jac_r_3 = p.calculateJacobian(self.arm3Uid, self.nJointsPerArm-1, com_pos, obs_lst[14:21], zero_vec, zero_vec)
         return jac_t_1, jac_t_2, jac_t_3
 
+    def get_arm_base_pose(self):
+        arm1_pos, arm1_rot = p.getBasePositionAndOrientation(self.arm1Uid)
+        arm2_pos, arm2_rot = p.getBasePositionAndOrientation(self.arm2Uid)
+        arm3_pos, arm3_rot = p.getBasePositionAndOrientation(self.arm3Uid)
+        return arm1_pos, arm1_rot, arm2_pos, arm2_rot, arm3_pos, arm3_rot
+
     def reset(self):
         self.step_counter = 0
         p.resetSimulation()
         p.configureDebugVisualizer(p.COV_ENABLE_RENDERING,0) # we will enable rendering after we loaded everything
+        p.resetDebugVisualizerCamera( cameraDistance=3, cameraYaw=30, cameraPitch=-32, cameraTargetPosition=[0,0,0])
         urdfRootPath=pybullet_data.getDataPath()
         p.setGravity(0,0,-10)
 
@@ -127,10 +133,6 @@ class MultiArmBoxEnv(gym.Env):
             p.setJointMotorControlArray(self.arm2Uid, range(self.nJointsPerArm), p.VELOCITY_CONTROL, forces=np.zeros(self.nJointsPerArm))
             p.setJointMotorControlArray(self.arm3Uid, range(self.nJointsPerArm), p.VELOCITY_CONTROL, forces=np.zeros(self.nJointsPerArm))
 
-            # p.setJointMotorControlArray(self.arm1Uid, range(self.nJointsPerArm), p.TORQUE_CONTROL, forces=np.zeros(self.nJointsPerArm))
-            # p.setJointMotorControlArray(self.arm2Uid, range(self.nJointsPerArm), p.TORQUE_CONTROL, forces=np.zeros(self.nJointsPerArm))
-            # p.setJointMotorControlArray(self.arm3Uid, range(self.nJointsPerArm), p.TORQUE_CONTROL, forces=np.zeros(self.nJointsPerArm))
-
         #create a base
         baseUid = p.loadURDF(os.path.join(urdfRootPath, "table_square/table_square.urdf"),useFixedBase=True)
 
@@ -141,6 +143,7 @@ class MultiArmBoxEnv(gym.Env):
         vuid = p.createVisualShape(p.GEOM_BOX, halfExtents = [half_ext]*3, rgbaColor=[0, 0, 1, 0.8])
         mass_box = 0.5
         self.objectUid = p.createMultiBody(mass_box,cuid,vuid, basePosition=state_object)
+        p.changeDynamics(self.objectUid, -1, lateralFriction=2.0, spinningFriction=0.8, rollingFriction=0.8)
 
         #get observations, only joint positions are considered
         arm1_joint_state = getMotorJointStates(self.arm1Uid)

tests/test_multiagent_obj_imp_ctrl.py
@@ -7,31 +7,25 @@ import gym
 from object_impedance_control.controllers.object_impedance_controller import MultiAgentObjectImpedanceController
 
 def main():
-    """
-    Tests importing of gym envs
-    """
+
     env = gym.make('multiagentbox-v0')
 
     env.reset()
 
-    controller = MultiAgentObjectImpedanceController(3, 80, 0, agent_link_stiffness=20)
+    controller = MultiAgentObjectImpedanceController(3, 80, 10, agent_link_stiffness=20)
     controller.set_desired_object_pos(np.array([0, 0, 1.2]))
 
     while True:
         state = env._get_state()
         agent_pos = np.reshape(state, (3, -1))[:, :3]
         act_forces = controller.step(agent_pos, agent_force=None)
-        # print(act_forces)
+
         env.step(act_forces.flatten())
 
         sleep(1./240.)
 
 
     return True
-
-
-def test_import():
-    assert main() is True
 
 
 if __name__ == '__main__':

tests/test_multiarm_obj_imp_ctrl.py
@@ -5,6 +5,7 @@ import numpy as np
 import gym
 
 from object_impedance_control.controllers.object_impedance_controller import MultiAgentObjectImpedanceController
+import object_impedance_control.controllers.utils as utils
 
 def main():
     """
@@ -14,14 +15,22 @@ def main():
 
     env.reset()
 
-    controller = MultiAgentObjectImpedanceController(3, 80, 0, agent_link_stiffness=20)
+    controller = MultiAgentObjectImpedanceController(3, 120, 10, agent_link_stiffness=30)
     controller.set_desired_object_pos(np.array([0, 0, 1.2]))
 
     while True:
         ee_states = env.get_forward_kinematics_pos()
         agent_pos = np.array(ee_states)
         act_forces = controller.step(agent_pos, agent_force=None)
-        # print(act_forces)
+
+        pos1, rot1, pos2, rot2, pos3, rot3 = env.get_arm_base_pose()
+        # get forces in the robot base reference frame
+        act_forces = np.array([
+            act_forces[0].dot(utils.quaternion_to_matrix(rot1)),
+            act_forces[1].dot(utils.quaternion_to_matrix(rot2)),
+            act_forces[2].dot(utils.quaternion_to_matrix(rot3))
+        ])
+
         arm1_jac, arm2_jac, arm3_jac = env.get_position_jacobian()
 
         arm1_jnt_trq = act_forces[0].dot(np.array(arm1_jac))
@@ -29,17 +38,11 @@ def main():
         arm3_jnt_trq = act_forces[2].dot(np.array(arm3_jac))
 
         env.step(np.array(np.concatenate([arm1_jnt_trq, arm2_jnt_trq, arm3_jnt_trq])))
-        # env.step(np.zeros(21))
-        # env.render()
 
         sleep(1./240.)
 
 
     return True
-
-
-def test_import():
-    assert main() is True
 
 
 if __name__ == '__main__':